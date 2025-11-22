4c8ec9f2aa1acf78547f4b45902cbdc39e059050
DOHOON <kkdh1215@gmail.com>
2025-11-23 06:52:58 +0900

    [feat] API_v2 완성

--- src/main/java/com/unveil/config/SecurityConfig.java
+++ src/main/java/com/unveil/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.unveil.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,12 +21,14 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // form login 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)
-                // http Basic 인증 비활성화 - jwt 구현 전 임시 활성화
-                // .httpBasic(AbstractHttpConfigurer::disable)
+                // http Basic 인증 비활성화 (브라우저 기본 아이디/비밀번호 팝업 방지)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 // URL별 권한 설정
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(
-                                "/api/users/signup",
+                                "/api/login",
+                                "/api/signup",
+                                "/api/locations/**",
                                 "/api/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
@@ -36,8 +37,8 @@ public class SecurityConfig {
                         ).permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults()); // 임시로 httpBasic 활성화
+                
+                );
 
         return http.build();
     }
