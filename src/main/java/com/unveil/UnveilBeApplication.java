24b420aa615d88f24db02e17cc06b563b6ec33de
DOHOON <kkdh1215@gmail.com>
2025-11-22 15:15:12 +0900

    [feat] 스프링 초기 세팅

---
+++ src/main/java/com/unveil/UnveilBeApplication.java
@@ -0,0 +1,13 @@
+package com.unveil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnveilBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnveilBeApplication.class, args);
+    }
+}
+
